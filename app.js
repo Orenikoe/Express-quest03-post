877d56b9ca15cb4ba2cd0ec495d1943d76634cb8
Orenikoe <95924002+Orenikoe@users.noreply.github.com>
2022-11-07 15:01:20 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const database = require('./db_connection.js')
+app.use(express.json());
+const port = 5000;
+
+
+
+app.get('/users', (req, res) => {
+	const { id } = req.params;
+	database
+		.query("SELECT * FROM users")
+		.then(([result]) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error adding the user');
+		});
+});
+
+app.post('/users', async (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+	await database
+		.query('INSERT INTO users(username, password) VALUES (?, ?)', [
+			username,
+			password,
+		])
+		.then(([result]) => {
+			// res.location(`/users/${result.insertId}`).sendStatus(201);
+	res.send(result);
+
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send('Error adding the user');
+		});
+
+
+});
+
+app.listen(port, (err) => {
+	if (err) {
+		console.error('something bad happened');
+	} else {
+		console.log('Server is listening');
+	}
+});
+
+
+
